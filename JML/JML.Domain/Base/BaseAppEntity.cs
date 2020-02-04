78e99827cf5bffe17bff4ea09d902a6b66c0b6d3
v.prokopchuk <vadim.prokopchuk@ocsico.com>
2020-02-04 20:40:53 +0300

    Base domain classes

---
+++ JML/JML.Domain/Base/BaseAppEntity.cs
@@ -0,0 +1,9 @@
+﻿using JML.Domain.Core.Contracts;
+
+namespace JML.Domain.Base
+{
+    public abstract class BaseAppEntity<TKey> : IAppEntity<TKey>
+    {
+        public TKey Id { get; protected set; }
+    }
+}
---
+++ JML/JML.Domain/Base/BaseAuditableEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using JML.Domain.Core.Contracts;
+
+namespace JML.Domain.Base
+{
+    public abstract class BaseAuditableEntity<TKey> : BaseAppEntity<TKey>, IAuditableEntity
+    {
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime ModifiedAt { get; protected set; }
+    }
+}
