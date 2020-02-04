a32d62d1ada1e983f052e8b7264b8000ec2ef935
v.prokopchuk <vadim.prokopchuk@ocsico.com>
2020-02-04 20:32:44 +0300

    Domain Core project

---
+++ JML/JML.Domain.Core/Contracts/IAppEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace JML.Domain.Core.Contracts
+{
+    public interface IAppEntity<TKey>
+    {
+        TKey Id { get; }
+    }
+}
---
+++ JML/JML.Domain.Core/Contracts/IAuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace JML.Domain.Core.Contracts
+{
+    public interface IAuditableEntity<TKey> : IAppEntity<TKey>
+    {
+        DateTime CreatedAt { get; }
+        DateTime ModifiedAt { get; }
+    }
+}
