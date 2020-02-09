80e03d6a1b7b248b8a989431da9bb615cef4e45b
Vadim <mailvadimprokopchuk@gmail.com>
2020-02-09 20:23:33 +0300

    User fields and groups (#2)

---
+++ JML/JML.Domain/Group.cs
@@ -0,0 +1,17 @@
+﻿using JML.Domain.Core.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace JML.Domain
+{
+    public class Group : IAppEntity<Guid>, IAuditableEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
---
+++ JML/JML.Domain/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using JML.Domain.Core.Contracts;
+
+namespace JML.Domain
+{
+    public class User : IAppEntity<Guid>, IAuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid? GroupId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public int CountOfInvalidAttempts { get; set; }
+        public bool IsLocked { get; set; }
+
+        public DateTime LoginAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+
+        public virtual Group Group { get; set; }
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+    }
+}
