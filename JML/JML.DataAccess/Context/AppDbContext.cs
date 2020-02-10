ce4524eda632067c037614e306421d2418e792d8
Vadim Prokopchuk <vadim.prokopchuk@ocsico.com>
2020-02-11 00:06:32 +0300

    Revert "fixed context, added factory"
    This reverts commit 76022e8cb5b6ad4c1e416afbb34799d768582f85.

--- JML/JML.DataAccess/Context/AppDbContext.cs
+++ JML/JML.DataAccess/Context/AppDbContext.cs
@@ -6,7 +6,10 @@ namespace JML.DataAccess.Context
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options) { }
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
