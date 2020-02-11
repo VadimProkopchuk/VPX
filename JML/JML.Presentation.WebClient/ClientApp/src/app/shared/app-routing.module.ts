9fafc0f7aefef6a135484a8ae54e7b34572e1fc3
Vadim Prokopchuk <vadim.prokopchuk@ocsico.com>
2020-02-11 21:19:02 +0300

    Clean solution

---
+++ JML/JML.Presentation.WebClient/ClientApp/src/app/shared/app-routing.module.ts
@@ -0,0 +1,31 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+/* import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {PostPageComponent} from './post-page/post-page.component';*/
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  /*{
+    /*th: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: HomePageComponent},
+      {path: 'post/:id', component: PostPageComponent}
+    ]
+
+    },
+    {
+      path: 'admin', loadChildren: './admin/admin.module#AdminModule'
+    }*/
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
